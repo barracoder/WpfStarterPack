42e3782f4fce7e15a62e05df697bbbe43bace915
barracoder <mark.green.external@rothschild.com>
2017-06-19 15:19:06 +0100

    Added WideWorldImporters and restore script

---
+++ src/Restore_WideWorldImporters.sql
@@ -0,0 +1,8 @@
+﻿RESTORE FILELISTONLY
+FROM DISK = 'C:\Users\Mark.Green\Downloads\WideWorldImporters-Standard.bak'
+RESTORE DATABASE WideWorldImporters
+FROM DISK = 'C:\Users\Mark.Green\Downloads\WideWorldImporters-Standard.bak'
+WITH MOVE 'WWI_Primary' TO 'C:\Users\Mark.Green\WideWorldImporters.mdf',
+MOVE 'WWI_UserData' TO 'C:\Users\Mark.Green\WideWorldImporters_UserData.ndf',
+MOVE 'WWI_Log' TO 'C:\Users\Mark.Green\WideWorldImporters.ldf',
+REPLACE;
